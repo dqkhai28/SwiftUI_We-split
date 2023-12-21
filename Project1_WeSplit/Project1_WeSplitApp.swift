0d6565baba345e5a408a3fbbeb5c4a38f3e1cf1a
BM Kane <kane@brickmate.vn>
2023-12-21 13:56:56 +0700

    Initial Commit

---
+++ Project1_WeSplit/Project1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project1_WeSplitApp.swift
+//  Project1_WeSplit
+//
+//  Created by Kane on 21/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Project1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
